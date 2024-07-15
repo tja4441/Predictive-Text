91368654995a58000149a2f8216e25518358bc9a
Timothy Avila <tja4441@rit.edu>
2024-07-15 19:03:28 -0400

    saving again

--- main.py
+++ main.py
@@ -2,32 +2,32 @@ import random
 
 # This program creates a predictive text model using n-gram analysis to suggest
 # words to follow a given reference word
-depth = 2
+depth = 3
 
-referenceText = "sources/beemovie.txt"
-textFile = open(referenceText)
-words = textFile.read().split()
+shrek = open("sources/shrek1.txt").read().split()
 
-window = []
-freqDict = {}
+dict = {}
 
-#Create frequency dictionary
-for word in words:
-    cleanWord = word.strip('.;,-“’”:?—‘!()_').lower()
-    if len(cleanWord) != 0:
-        window.append(cleanWord)
-        if len(window) > depth + 1:
-            window.pop(0)
-        if len(window) > depth:
-            key = tuple(window[0:-1])
-            val = window[len(window)-1]
+def analyze(freqDict, words):
+    window = []
+    #Create frequency dictionary
+    for word in words:
+        cleanWord = word.strip('.;,-“’”:?—‘!()_').lower()
+        if len(cleanWord) != 0:
+            window.append(cleanWord)
+            if len(window) > depth + 1:
+                window.pop(0)
+            if len(window) > depth:
+                key = tuple(window[0:-1])
+                val = window[len(window)-1]
+                if key not in freqDict:
+                    freqDict[key] = [val]
+                else:
+                    freqDict[key].append(val)
 
-            if key not in freqDict:
-                freqDict[key] = [val]
-            else:
-                freqDict[key].append(val)
+analyze(dict, shrek)
 
-current = ("and","then")
-for i in range(10):
-    print(current[0])
-    current = (current[1],random.choice(freqDict[current]))
+current = ("i'm","going","to")
+for i in range(20):
+    print(current[0],end=" ")
+    current = (current[1],current[2],random.choice(dict[current]))
